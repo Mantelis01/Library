cc6614b968b58cb9884688c954131db92f6c9bb0
Mantelis01 <mantas.zukovskis@gmail.com>
2022-10-13 23:45:23 +0300

    updated structure

---
+++ code.js
@@ -0,0 +1,10 @@
+function lentele() {
+    document.getElementById("form").style.display = "flex";
+}
+
+function Book (){
+    this.name = name;
+    this.author = author;
+    this.pages= pageNum;
+    this.status=status;
+}
